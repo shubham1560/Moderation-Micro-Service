cf93f80daef858231b9a413e9648ac4a3a748b81
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-08-16 09:30:27 +0530

    Completed

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+
+
+app.post("/events", (req, res) =>{
+    commentData = req.body;
+    commentData.data.status = "rejected";
+    // console.log(commentData.data);
+    axios.post('http://localhost:4005/events', {
+        "type": "CommentModerated",
+        "data": commentData,
+    })
+})
+
+app.listen(4003, ()=>{
+    console.log("moderation listening at port 4003");
+})
